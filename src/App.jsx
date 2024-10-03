7dc4ab51c4e946484ff07cb1af724dd60e3fe548
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-10-03 10:28:49 +0100

    fixed app.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useRef } from 'react'
+import './index.css'
+import './App.css'
+import { Routes, Route, Link, useNavigate } from 'react-router-dom'
+import Home from './pages/Home'
+import Checkout from './pages/Checkout'
+
+const App = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App
